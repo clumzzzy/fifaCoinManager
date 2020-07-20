5d623c3e10939f6299256e3ad372b953fcf122da
clumzzzy <Aartem932@gmail.com>
2020-07-20 23:23:32 +0300

    Initial Commit

---
+++ fifaCoinManagerTests/fifaCoinManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  fifaCoinManagerTests.swift
+//  fifaCoinManagerTests
+//
+//  Created by Artem Tyumencev on 20.07.2020.
+//  Copyright © 2020 Artem Tyumencev. All rights reserved.
+//
+
+import XCTest
+@testable import fifaCoinManager
+
+class fifaCoinManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
