9cedb4d4298b5a01410cc9cf9c7bf169f27e2f8d
clumzzzy <Aartem932@gmail.com>
2020-08-19 22:32:31 +0300

    Added names to all controllers and improved MVC

---
+++ fifaCoinManager/ Main info screen/Controller/MainInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainInfoViewController.swift
+//  fifaCoinManager
+//
+//  Created by Artem Tyumencev on 19.08.2020.
+//  Copyright © 2020 Artem Tyumencev. All rights reserved.
+//
+
+import UIKit
+
+class MainInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
